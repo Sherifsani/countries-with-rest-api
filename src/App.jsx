807f980cc4dc5f4b86a4705bc3b6bbb6771c6bfb
sherifsani <sanisherif11@gmail.com>
2023-07-12 22:01:41 +0100

    added the filter by region option

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
 import Selectmenu from "./components/SelectMenu";
 import { useState, useEffect } from "react";
-import CountryCard from "./components/CountryCard";
 import data from "./data.json";
 
 function App() {
@@ -18,52 +17,64 @@ function App() {
     }
   }, [theme]);
 
-  function handleSearch(e){
-    setSearch(e.target.value)
+  function handleSearch(e) {
+    setSearch(e.target.value);
+    console.log(search);
   }
   function changeTheme() {
     setTheme(theme === "dark" ? "light" : "dark");
     console.log(theme);
   }
 
-  const dataElem = data.map((item) => (
-    <div className="country-card bg-White dark:bg-DBDME flex flex-col gap-2 h-[330px] items-start w-full rounded-md overflow-hidden shadow-md  ">
-      <img src={item.flags.png} alt="" className="image" />
-      <h3 className="font-semibold px-7 mt-5 dark:text-white">{item.name}</h3>
-      <div className="details flex flex-col px-7">
-        <p>
-          <span className="text-sm font-semibold dark:text-white">
-            population:{" "}
-          </span>
-          <span className="text-sm dark:text-white">{item.population}</span>
-        </p>
-        <p>
-          <span className="text-sm font-semibold dark:text-white">
-            Region:{" "}
-          </span>
-          <span className="text-sm dark:text-white">{item.region}</span>
-        </p>
-        <p>
-          <span className="text-sm font-semibold dark:text-white">
-            Capital:{" "}
-          </span>
-          <span className="text-sm dark:text-white">{item.capital}</span>
-        </p>
+  const dataElem = data
+    .filter((item) => {
+      return search.toLowerCase() === ""
+        ? item
+        : item.name.toLowerCase().includes(search);
+    })
+    .map((item, index) => (
+      <div
+        key={index}
+        className="country-card bg-White cursor-pointer dark:bg-DBDME flex flex-col gap-2 h-[330px] items-start w-full rounded-md overflow-hidden shadow-md  "
+      >
+        <img src={item.flags.png} alt="" className="image" />
+        <h3 className="font-semibold px-7 mt-5 dark:text-white">{item.name}</h3>
+        <div className="details flex flex-col px-7">
+          <p>
+            <span className="text-sm font-semibold dark:text-white">
+              population:{" "}
+            </span>
+            <span className="text-sm dark:text-white">{item.population}</span>
+          </p>
+          <p>
+            <span className="text-sm font-semibold dark:text-white">
+              Region:{" "}
+            </span>
+            <span className="text-sm dark:text-white">{item.region}</span>
+          </p>
+          <p>
+            <span className="text-sm font-semibold dark:text-white">
+              Capital:{" "}
+            </span>
+            <span className="text-sm dark:text-white">{item.capital}</span>
+          </p>
+        </div>
       </div>
-    </div>
-  ));
+    ));
 
   return (
-    <div className="flex flex-col items-center gap-8">
-      <Navbar onclick={changeTheme} theme={theme} />
-      <main className="main w-full px-4 flex flex-col gap-8">
-        <div className="search-select w-full  search-select mt-5 flex gap-5 flex-col items-start md:flex-row md:items-center md:justify-between max-w-[1300px]">
-          <SearchBar change={handleSearch} search={search}/>
-          <Selectmenu/>
-        </div>
-        <div className="card-wrapper w-full max-w-7xl">{dataElem}</div>
-      </main>
-    </div>
+    <>
+      <div className="flex flex-col items-center gap-8">
+        <Navbar onclick={changeTheme} theme={theme} />
+        <main className="main w-full px-4 flex flex-col gap-8">
+          <div className="search-select w-full  search-select mt-5 flex gap-5 flex-col items-start md:flex-row md:items-center md:justify-between max-w-[1300px]">
+            <SearchBar change={handleSearch} search={search} />
+            <Selectmenu />
+          </div>
+          <div className="card-wrapper w-full max-w-7xl">{dataElem}</div>
+        </main>
+      </div>
+    </>
   );
 }
 
